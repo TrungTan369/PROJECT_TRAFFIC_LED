fceb1a7b0c2895d9cffa9392c27bfdd51e739a1d
TrungTan <ttqpentakill@gmail.com>
2024-11-08 11:46:08 +0700

    m

--- Core/Src/fsm_slow.c
+++ Core/Src/fsm_slow.c
@@ -7,27 +7,24 @@
 
 
 void fsm_slow_run(){
-	switch(status){
-	case SLOW:
+	if(status == SLOW){
 		if(timer_flag[0]==1){
 			HAL_GPIO_TogglePin(Y0_GPIO_Port, Y0_Pin);
 			HAL_GPIO_TogglePin(Y1_GPIO_Port, Y1_Pin);
 			set_timer(0, 500);
-			}
-		break;
-	default:
+		}
+		if(isButtonPress(0)==1){
+			lcd_clear_display();
+			lcd_goto_XY(1, 0);
+			lcd_send_string("MODE: AUTO      ");
+			lcd_goto_XY(0, 0);
+			lcd_send_string("PLEASE SLOW DOWN");
+			status = auto_red_green;
+			count0 = (time_red_green + time_red_yellow)/1000;
+			count1 = time_red_green / 1000;
+			setTimer(0, time_red_green);
+		}
+	}
+	else
 		return;
-
-	}
-	if(isButtonPress(0)==1){
-		lcd_clear_display();
-		lcd_goto_XY(1, 0);
-		lcd_send_string("MODE: AUTO      ");
-		lcd_goto_XY(0, 0);
-		lcd_send_string("PLEASE SLOW DOWN");
-		status = auto_red_green;
-		count0 = (time_red_green + time_red_yellow)/1000;
-		count1 = time_red_green / 1000;
-		setTimer(0, time_red_green);
-	}
 }
